64c3c06a1edab0c73e2feeaaf3cca20afeab99c6
JΛN <jonnygem7@gmail.com>
2025-11-16 22:13:24 +0100

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+    kotlin("jvm") version "2.2.21"
+    id("com.gradleup.shadow") version "9.2.0"
+    id("xyz.jpenilla.run-paper") version "3.0.2"
+}
+
+group = "de.doetchen"
+version = "0.1"
+
+repositories {
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/") {
+        name = "papermc-repo"
+    }
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.21-R0.1-SNAPSHOT")
+}
+
+tasks {
+    runServer {
+        minecraftVersion("1.21")
+    }
+}
+
+val targetJavaVersion = 21
+kotlin {
+    jvmToolchain(targetJavaVersion)
+}
+
+tasks.build {
+    dependsOn("shadowJar")
+}
+
+tasks.processResources {
+    val props = mapOf("version" to version)
+    inputs.properties(props)
+    filteringCharset = "UTF-8"
+    filesMatching("paper-plugin.yml") {
+        expand(props)
+    }
+}
---
+++ src/main/kotlin/de/doetchen/project/Needed.kt
@@ -0,0 +1,14 @@
+package de.doetchen.project
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Needed : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
