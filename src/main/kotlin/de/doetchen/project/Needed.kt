f744ac4275cf9c5c1a7c97090e9287b233b787d3
JΛN <jonnygem7@gmail.com>
2025-11-17 18:07:17 +0100

    Update Needed.kt

--- src/main/kotlin/de/doetchen/project/Needed.kt
+++ src/main/kotlin/de/doetchen/project/Needed.kt
@@ -5,10 +5,8 @@ import org.bukkit.plugin.java.JavaPlugin
 class Needed : JavaPlugin() {
 
     override fun onEnable() {
-        // Plugin startup logic
     }
 
     override fun onDisable() {
-        // Plugin shutdown logic
     }
 }
